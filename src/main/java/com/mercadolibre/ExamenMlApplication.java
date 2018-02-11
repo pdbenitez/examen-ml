37a03dd1fadfb85736928674201d0f264d731ce1
pdbenitez <pdanibenitez91@gmail.com>
2018-02-11 20:49:49 -0300

    Initial Project

---
+++ src/main/java/com/mercadolibre/ExamenMlApplication.java
@@ -0,0 +1,12 @@
+package com.mercadolibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMlApplication.class, args);
+	}
+}
